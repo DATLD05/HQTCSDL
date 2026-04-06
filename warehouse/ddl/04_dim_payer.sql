8866fea7dadc8f69541d114e5f5c6c70e0f31c66
LDH1401 <hungleduy278@gmail.com>
2026-04-06 12:30:50 +0700

    rm

--- warehouse/ddl/04_dim_payer.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `your_project.your_dataset.dim_payer` (
-  payer_key INT64,
-  payer_id STRING,
-  payer_name STRING,
-  ownership STRING,
-  payer_city STRING,
-  payer_state STRING
-)
-CLUSTER BY payer_id;
--- warehouse/ddl/05_dim_encounter_type.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `your_project.your_dataset.dim_encounter_type` (
-  encounter_type_key INT64,
-  encounter_class STRING,
-  encounter_description STRING
-)
-CLUSTER BY encounter_class;
